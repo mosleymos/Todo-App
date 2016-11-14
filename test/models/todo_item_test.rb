1638d68b5868fee18fe454b93a7076eb7809ba52
keita mori <keitamori@gmail.com>
2016-11-15 00:16:38 +0100

    More tests on models - initialization

--- test/models/todo_item_test.rb
+++ test/models/todo_item_test.rb
@@ -22,4 +22,16 @@ class TodoItemTest < ActiveSupport::TestCase
     simple_todo_item = TodoItem.create(content: 'Buy some mustard', todo_list_id: simple_todo_list.id)
     assert_equal 3,  TodoItem.count
   end
+
+  test '#new' do
+    simple_todo_item = TodoItem.new
+    assert simple_todo_item.respond_to? :content
+    assert simple_todo_item.respond_to? :todo_list_id
+  end
+
+  test '#completed?' do
+    simple_todo_list = todo_lists(:groceries_list)
+    simple_todo_item = TodoItem.create(content: 'Buy some mustard', todo_list_id: simple_todo_list.id)
+    assert_equal false, simple_todo_item.completed?
+  end
 end
