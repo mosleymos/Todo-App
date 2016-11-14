5ab053a59e7d8210017e77115119c7d54431cc7f
keita mori <keitamori@gmail.com>
2016-11-14 19:21:34 +0100

    Annotate documentation and validates presence of content on task

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,22 @@
+# == Schema Information
+#
+# Table name: todo_items
+#
+#  id           :integer          not null, primary key
+#  content      :string(255)
+#  todo_list_id :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#  completed_at :datetime
+#
+# Indexes
+#
+#  index_todo_items_on_todo_list_id  (todo_list_id)
+#
+
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
+  validates :content, presence: true
 
   def completed?
   	!completed_at.blank?
--- test/models/todo_list_test.rb
+++ test/models/todo_list_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: todo_lists
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'test_helper'
 
 class TodoListTest < ActiveSupport::TestCase
