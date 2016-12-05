ac971f9bc0873cbd3cbb578381d85db976d91e71
keita mori <keitamori@gmail.com>
2016-12-05 22:22:06 +0100

    Rails Garberish technique included

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 var $ = require('jquery')
+var _ = require('underscore')
 
+var log = function(something){ return console.log(something) }
 $(function(){
-  console.log('ok workin js compilation')
-  console.log('ok workin rails integration')
+  var ActionName = $('#js_mgnt').data('controller-action')
+  var ActionController = $('#js_mgnt').data('controller-name')
+  log(ActionName)
+  log(ActionController)
 })
